76bfe321aece1b48539ae23f44fd96d31a30b2df
pawidachum <pawida.may@gmail.com>
2021-03-15 13:55:11 +0700

    test code angular material finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
